a0f663a52365346aeea76a8567fb8602b78fe2e4
Nico Ramirez <nramirez@senapi.gob.bo>
2023-07-13 11:41:32 -0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'x',
+    authDomain: 'x',
+    projectId: 'x',
+    storageBucket: 'x',
+    messagingSenderId: 'x',
+    appId: 'x',
+    measurementId: 'x'
+  }
+};
